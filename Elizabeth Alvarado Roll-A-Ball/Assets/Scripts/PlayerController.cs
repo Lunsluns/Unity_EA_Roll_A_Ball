cdefcf816c0ac35dfa9d7e785509df68ef17003b
Elizabeth "Elise" Alvarado <danceswithtime@gmail.com>
2019-01-18 03:29:53 -0600

    Initial commit

---
+++ Elizabeth Alvarado Roll-A-Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    //text
+    public Text countText;
+    public Text winText;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float move_Horizontal = Input.GetAxis("Horizontal");
+        float move_Vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(move_Horizontal, 0.0f, move_Vertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
